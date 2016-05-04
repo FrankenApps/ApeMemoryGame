e0d49163d00139e46dca38c56c5f1f1c68f3d6e6
FrankenApps <de.frankenapps@gmail.com>
2016-05-04 09:56:50 +0200

    Completed UI

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ApeMemoryGame
 
-This is a little web project with Javascript and Jquery. You must remember the Numbers, that are then hidden by the white squares. Your task is to remember where each number was and click on the corresponding square. You can look up the score of chimpanzees in that game and try to beat them.
+This is a little web project with Javascript and Jquery. You must try to remember the Numbers, that are then hidden by the white squares. Your task is to remember where each number was and click on the corresponding square. You can look up the score of chimpanzees in that game and try to beat them.
 
-Note: Still under heavy development.
+Note: Still nothing working, but under heavy development.
--- game_logic.js
+++ game_logic.js
@@ -1,10 +1,12 @@
 var timeMode = true;
+lang = "en";
 
 $(document).ready(function() {
   $("#time_mode_settings").slideDown(400);
   timeMode=true;
   $("#time_mode_radio").prop("checked", true)
   console.log("TimeMode: " + timeMode);
+
   //radios
   $('input[type=radio][name=mode]').change(function() {
     if (this.id == 'time_mode_radio') {
@@ -17,13 +19,59 @@ $(document).ready(function() {
     }
   });
 
+  //create the game field buttons
+  for (var i = 0; i < 40; i++) {
+    var r = $('<div style="float: left" id="'+ "buttondiv"+ String(i) +'"><button style="width: 100px; height: 100px; background-color: #000000; color: #ffffff; font-size : 60px; border:none;" id="' + "gameButton" + String(i)+'"></button>' + '</div>');
+    $("#playground").append(r);
+    console.log(i);
+  }
+
   //buttons
   $("button").on('click', function(){
     if (this.id=="close_button") {
 
     }
     else if (this.id=="start_game_button") {
-
+      startTheGame();
+    }
+    else {
+      //game logic buttons
     }
   });
 });
+
+function startTheGame() {
+  if(timeMode){
+    if ($("#fade_in_input").val()<1 || $("fade_out_input").val()<1) {
+      showInputErrorWarning();
+    }
+    else {
+      generateNumbers();
+    }
+  }
+  else {
+
+  }
+}
+
+function showInputErrorWarning() {
+  if (lang=="en") {
+    alert("You have to enter a numeric value in both text fields.");
+  }
+  else if (lang=="de") {
+    alert("Sie müssen eine Zahl in beide Textfelder eingeben.")
+  }
+}
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+function generateNumbers(){
+  
+}

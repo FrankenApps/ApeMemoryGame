50c362e4de032672ddf91b30961d69e8efcf397e
FrankenApps <de.frankenapps@gmail.com>
2016-05-05 10:28:04 +0200

    Making it more mobile friendly

--- game_logic.js
+++ game_logic.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     $('#fade_in_input').val('1500');
     $('#fade_in_input').css('margin-left', '3px');
     $('#time_mode_radio').css('margin-top', '8px');
-    $('fade_in_input').css('margin-left', '20px');
+    $('fade_in_input').css('margin-left', '2000px');
     $('#time_mode_settings').css('margin-bottom', '4px');
   }
 
